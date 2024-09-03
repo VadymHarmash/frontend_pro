141743b709fbae6bfdae5e32b373f4a1695c63a8
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-09-03 22:02:52 +0300

    completed homework 3.2

---
+++ frontend_pro/index.js
@@ -0,0 +1,5 @@
+const firstPrompt = prompt("What is your name?");
+const secondPrompt = prompt("How old are you");
+const thirdPrompt = prompt("Where are you from?");
+
+console.log(`Your name is ${firstPrompt}, you are ${secondPrompt} years old, and you live in ${thirdPrompt}`);
