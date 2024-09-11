c533f32da9299168d04e4cd80cfdc975eb9a5edb
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-09-11 10:00:34 +0300

    completed homework 5.2

--- frontend_pro/index.js
+++ frontend_pro/index.js
@@ -1,5 +1,18 @@
 const oneDollar = 26;
 
+//for
+console.log('For');
+
 for (let i = 10; i <= 100; i += 10) {
     console.log(`${i} dollars are equal to ${oneDollar * i} hryvnas`);
+}
+
+//while
+console.log('While');
+
+let i = 10;
+
+while (i <= 100) {
+    console.log(`${i} dollars are equal to ${oneDollar * i} hryvnas`);
+    i += 10;
 }
