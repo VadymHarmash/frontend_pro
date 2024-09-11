3b76b0abf46ec190b0e7818c87d480e506fd1b87
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-09-11 10:06:42 +0300

    completed homework 5.3

--- frontend_pro/index.js
+++ frontend_pro/index.js
@@ -1,18 +1,22 @@
 const userNumber = +prompt("Enter your number");
 
-//for
-console.log('For');
-for(let i = 0; i < userNumber; i++) {
-    if(i**2 > userNumber) break;
-    console.log(i);
-}
+if (isNaN(userNumber) || !userNumber) {
+    console.log('It is not a number.');
+} else {
+    //for
+    console.log('For');
+    for (let i = 0; i < userNumber; i++) {
+        if (i ** 2 > userNumber) break;
+        console.log(i);
+    }
 
-// while
-console.log('While');
-let i = 0;
+    // while
+    console.log('While');
+    let i = 0;
 
-while(i < userNumber) {
-    if(i**2 > userNumber) break;
-    console.log(i);
-    i++;
+    while (i < userNumber) {
+        if (i ** 2 > userNumber) break;
+        console.log(i);
+        i++;
+    }
 }
