5c9a54181c28789d95b5b27cc5b02489b0fe7300
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-09-11 09:50:43 +0300

    completed homework 5.1

--- frontend_pro/index.js
+++ frontend_pro/index.js
@@ -1,7 +1,20 @@
+//for
+console.log('For');
 let finalString = '';
 
-for(let i = 20; i <= 30; i+=0.5){
+for (let i = 20; i <= 30; i += 0.5) {
     finalString += `${i} `;
-};
+}
+console.log(`${finalString.trim()}`);
 
+//while
+console.log('While');
+finalString = '';
+
+let i = 20;
+
+while (i <= 30) {
+    finalString += `${i} `;
+    i += 0.5;
+}
 console.log(`${finalString.trim()}`);
