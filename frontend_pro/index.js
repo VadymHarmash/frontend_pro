a58e0e8478e3947f5bc254c83e62659e47849863
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-09-18 13:54:57 +0300

    completed homework 7.3

--- frontend_pro/index.js
+++ frontend_pro/index.js
@@ -1,19 +1,16 @@
-let userNumber = +prompt("Attempt 1. number greater then 100");
+let userNumber = +prompt('Attempt 1. number greater than 100');
 let attempt = 1;
 
 while (attempt < 10) {
     attempt++;
     if (isNaN(userNumber) || !userNumber) {
-        userNumber = +prompt(`Attempt ${attempt}. It is not a number.`);
+        userNumber = prompt(`Attempt ${attempt}. It is not a number.`);
+        if (attempt === 10) console.log(isNaN(+userNumber) ? userNumber : +userNumber);
     } else if (userNumber <= 100) {
-        userNumber = +prompt(`Attempt ${attempt}. Number is less then 100`);
+        userNumber = +prompt(`Attempt ${attempt}. Number is less than 100`);
+        if (attempt === 10) console.log(userNumber);
     } else {
-        console.log(userNumber);
-        break
-    }
-
-    if(attempt === 10) {
-        console.log(userNumber);
+        console.log(+userNumber);
         break;
     }
 }
