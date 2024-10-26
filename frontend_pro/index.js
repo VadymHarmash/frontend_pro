625286bd4ce0a4eb9771ac9add8ab78f7d6d6941
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-10-26 12:11:11 +0300

    completed homework 17.1

---
+++ frontend_pro/index.js
@@ -0,0 +1,23 @@
+class Calculator {
+  add (firstNum, secondNum) {
+    return firstNum + secondNum;
+  }
+
+  subtract (firstNum, secondNum) {
+    return firstNum - secondNum;
+  }
+
+  multiply (firstNum, secondNum) {
+    return firstNum * secondNum;
+  }
+
+  divide (firstNum, secondNum) {
+    return firstNum / secondNum;
+  }
+}
+
+const calc = new Calculator();
+console.log(calc.add(5, 3));
+console.log(calc.subtract(10, 4));
+console.log(calc.multiply(3, 6));
+console.log(calc.divide(8, 2));
