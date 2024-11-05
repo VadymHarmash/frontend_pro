cc358e54a963e55136f002c0a4bdc6bff14a3a67
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-11-05 09:50:16 +0200

    completed homework 19.1

--- frontend_pro/index.js
+++ frontend_pro/index.js
@@ -1,15 +1,26 @@
 const api = `https://api.openweathermap.org/data/2.5/weather?q=Odesa&appid=6a00609d6ada2701cc774188f7fadf1b`;
 const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-const date = new Date();
+// time updating
+let weatherTimeInterval;
+function updateTime() {
+  const date = new Date();
+  const dayName = days[date.getDay()];
+  const day = String(date.getDate()).padStart(2, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
 
-const dayName = days[date.getDay()];
-const day = String(date.getDate()).padStart(2, '0');
-const month = String(date.getMonth() + 1).padStart(2, '0');
-const hours = String(date.getHours()).padStart(2, '0');
-const minutes = String(date.getMinutes()).padStart(2, '0');
-getWeatherData();
+  weatherTime.innerHTML = `${dayName}, ${day}.${month}, ${hours}:${minutes}:${seconds}`;
+}
 
+function startUpdatingTime() {
+  updateTime();
+  weatherTimeInterval = setInterval(updateTime, 1000);
+}
+
+// elements creation
 const weatherWrapper = document.createElement('div');
 weatherWrapper.classList.add('weather__wrapper');
 
@@ -40,6 +51,9 @@ weatherPressure.classList.add('weather__pressure');
 const weatherWindSpeed = document.createElement('p');
 weatherWindSpeed.classList.add('weather__speed');
 
+const weatherFooter = document.createElement('div');
+weatherFooter.classList.add('weather__footer');
+
 const weatherState = document.createElement('p');
 weatherState.classList.add('weather__state');
 
@@ -58,6 +72,7 @@ weatherRefreshButton.addEventListener('click', () => {
   rotateRefreshIcon();
 });
 
+//refresh button animation
 let rotationAngle = 0;
 
 function rotateRefreshIcon(){
@@ -66,12 +81,15 @@ function rotateRefreshIcon(){
   weatherRefreshButtonIcon.style.transition = '1s ease-in-out';
 }
 
+//get weather data
+getWeatherData();
+
 function getWeatherData() {
   fetch(api)
     .then(response => response.json())
     .then(data => {
       weatherCity.innerHTML = `${data.name}, ${data.sys.country}`;
-      weatherTime.innerHTML = `${dayName}, ${day}.${month}, ${hours}:${minutes}`;
+      startUpdatingTime();
       weatherTemp.innerHTML = `Temperature: ${Math.round(data.main.temp - 273)}°C`;
       weatherFeelsLike.innerHTML = `Feels like ${Math.round(data.main.feels_like - 273)}°C`;
       weatherHumidity.innerHTML = `Humidity: ${data.main.humidity}%`;
@@ -80,7 +98,7 @@ function getWeatherData() {
       weatherState.innerHTML = `Status: ${data.weather[0].main}`;
       weatherImage.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
     })
-    .catch(error => console.error('ERROR:', error));
+    .catch(error => console.log('ERROR:', error));
 }
 
 weatherHeader.appendChild(weatherCity);
@@ -92,10 +110,12 @@ weatherInfo.appendChild(weatherFeelsLike);
 weatherInfo.appendChild(weatherHumidity);
 weatherInfo.appendChild(weatherPressure);
 weatherInfo.appendChild(weatherWindSpeed);
-weatherInfo.appendChild(weatherState);
-weatherInfo.appendChild(weatherImage);
+
+weatherFooter.appendChild(weatherState);
+weatherFooter.appendChild(weatherImage);
 
 weatherWrapper.appendChild(weatherHeader);
 weatherWrapper.appendChild(weatherInfo);
+weatherWrapper.appendChild(weatherFooter);
 
 document.body.appendChild(weatherWrapper);
