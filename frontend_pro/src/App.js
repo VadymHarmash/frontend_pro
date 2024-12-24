855b31e411f5f38ba2d869c66289f28060477c2a
Vadym_Harmash_ <vadimvlad20032002@gmail.com>
2024-12-24 11:05:15 +0200

    completed homework 32.1

--- frontend_pro/src/App.js
+++ frontend_pro/src/App.js
@@ -2,18 +2,15 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/header";
 import Routers from "./routers";
-import ThemeProvider from "./context/ThemeContext";
 import ContactsList from "./components/contactsList";
 
 function App() {
   return (
-    <ThemeProvider>
-      <BrowserRouter>
-        <Header />
-        <Routers />
-        <ContactsList />
-      </BrowserRouter>
-    </ThemeProvider>
+    <BrowserRouter>
+      <Header />
+      <Routers />
+      <ContactsList />
+    </BrowserRouter>
   );
 }
 
--- frontend_pro/src/components/header/index.jsx
+++ frontend_pro/src/components/header/index.jsx
@@ -1,16 +1,26 @@
-import React, { useContext } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
-import { AppBar, Toolbar, Typography, Button, IconButton, MenuItem, Box } from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  Button,
+  IconButton,
+  MenuItem,
+  Box,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
-import { ThemeContext } from "../../context/ThemeContext";
 
 export default function Header() {
-  const { theme, toggleTheme } = useContext(ThemeContext);
-
   return (
     <AppBar position="static">
       <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+        <IconButton
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        >
           <MenuIcon />
         </IconButton>
         <Typography variant="h6">My Website</Typography>
@@ -23,22 +33,13 @@ export default function Header() {
             gap: 2,
           }}
         >
-          <NavLink
-            to="/"
-            style={{ textDecoration: "none" }}
-          >
-            <MenuItem sx={{ color: '#FFFFFF' }}>About Me</MenuItem>
+          <NavLink to="/" style={{ textDecoration: "none" }}>
+            <MenuItem sx={{ color: "#FFFFFF" }}>About Me</MenuItem>
           </NavLink>
-          <NavLink
-            to="/todos"
-            style={{ textDecoration: "none" }}
-          >
-            <MenuItem sx={{ color: '#FFFFFF' }}>Todos</MenuItem>
+          <NavLink to="/todos" style={{ textDecoration: "none" }}>
+            <MenuItem sx={{ color: "#FFFFFF" }}>Todos</MenuItem>
           </NavLink>
         </Box>
-        <Button color="inherit" onClick={toggleTheme}>
-          {theme === "light" ? "Dark Mode" : "Light Mode"}
-        </Button>
       </Toolbar>
     </AppBar>
   );
